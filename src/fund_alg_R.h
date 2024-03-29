b5792113d538c42cc930fcd7d4ddf69f8d0830f7
DylanBahia <40526407+DylanBahia@users.noreply.github.com>
2024-03-29 11:54:41 +0000

    Update fund_alg_R.h

--- src/fund_alg_R.h
+++ src/fund_alg_R.h
@@ -3,6 +3,7 @@
 #include <unordered_map>
 #include <algorithm>
 #include <iterator>
+#include <list>
 #include "is_stable_funs.h"
 #include "fund_alg_funs.h"
 #include "marshall.h"
@@ -10,5 +11,5 @@
 #include "Rcpp.h"
 using namespace Rcpp;
 
-std::vector<std::vector<int>> fund_alg_r(std::vector<std::vector<int>>,
-					 std::vector<std::vector<int>>);
+std::vector<std::vector<int>> fund_alg_r(std::list<std::vector<int>>,
+					 std::list<std::vector<int>>);
