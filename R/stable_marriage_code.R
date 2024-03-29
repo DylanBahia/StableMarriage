692ad058c3f2c8ad539f5532b5d838257b9bed91
DylanBahia <40526407+DylanBahia@users.noreply.github.com>
2024-03-29 13:37:43 +0000

    Update stable_marriage_code.R

--- R/stable_marriage_code.R
+++ R/stable_marriage_code.R
@@ -1,14 +1,8 @@
 library(Rcpp)
 loadModule("fundamental",TRUE)
 
-post_process <- function(matching){
-  match_mat <- do.call(rbind,matching)
-  colnames(match_mat) <- c("Man","Woman")
-  rownames(match_mat) <- rep("",nrow(match_mat))
-  return(match_mat)
-}
 sm <- function(men,women){
-  stuff <- post_process(stable_marriage(men,women))
+  stuff <- stable_marriage(men,women)
   print("A")
   return(stuff)
 }
