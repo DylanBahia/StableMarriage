56ba75909fab930ab941bb4179b67177f7d18709
DylanBahia <40526407+DylanBahia@users.noreply.github.com>
2024-03-29 14:44:46 +0000

    Add files via upload

--- R/stable_marriage_code.R
+++ R/stable_marriage_code.R
@@ -2,10 +2,12 @@ library(Rcpp)
 loadModule("fundamental",TRUE)
 
 sm <- function(men,women){
+  print(men)
   name_change <- pre_process(men,women)
   men <- name_change[1]
   women <- name_change[2]
   dict <- name_change[3]
+  print(men)
   return(post_process(stable_marriage(men,women),dict))
 }
 
