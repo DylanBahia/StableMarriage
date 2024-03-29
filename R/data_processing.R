227a78e96985603eab66b87b2533746711a3d40f
DylanBahia <40526407+DylanBahia@users.noreply.github.com>
2024-03-29 14:43:21 +0000

    Add files via upload

--- R/data_processing.R
+++ R/data_processing.R
@@ -1,6 +1,29 @@
-post_process <- function(matching){
+post_process <- function(matching,dict){
   match_mat <- do.call(rbind,matching)
+  for(i in nrow(match_mat)){
+    for(j in ncol(match_mat)){
+      match_mat[i,j] <- dict[dict[,1]==match_mat[i,j]][2]
+    }
+  }
+  
   colnames(match_mat) <- c("Man","Woman")
   rownames(match_mat) <- rep("",nrow(match_mat))
+  
   return(match_mat)
+}
+
+pre_process <- function(men,women){
+  w_names <- men[[1]][-1]
+  m_names <- women[[1]][-1]
+  n <- length(w_names)
+  
+  dict <- cbind(seq(1,2*n),c(m_names,w_names))
+  
+  for(i in 1:n){
+    for(j in 1:n){
+      men[[i]][j] <- dict[dict[,2]==men[[i]][j]][1]
+      women[[i]][j] <- dict[dict[,2]==women[[i]][j]][1]
+    }
+  }
+  return(list(men=men,women=women,dict=dict))
 }
--- R/stable_marriage_code.R
+++ R/stable_marriage_code.R
@@ -2,6 +2,10 @@ library(Rcpp)
 loadModule("fundamental",TRUE)
 
 sm <- function(men,women){
-  return(post_process(stable_marriage(men,women)))
+  name_change <- pre_process(men,women)
+  men <- name_change[1]
+  women <- name_change[2]
+  dict <- name_change[3]
+  return(post_process(stable_marriage(men,women),dict))
 }
 
