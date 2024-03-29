8d8ecd43e9c32950de2d6dfe4e467359ded20aef
DylanBahia <40526407+DylanBahia@users.noreply.github.com>
2024-03-29 11:54:57 +0000

    Update marshall.h

--- src/marshall.h
+++ src/marshall.h
@@ -2,5 +2,6 @@
 #include <unordered_map>
 #include <algorithm>
 #include <iterator>
+#include <list>
 
-std::unordered_map<int,std::vector<int>> marshall(std::vector<std::vector<int>>);
+std::unordered_map<int,std::vector<int>> marshall(std::list<std::vector<int>>);
